c1f118ae50156a6309a494809b2dcb08f9f807aa
ronamit <amitron5@gmail.com>
2020-03-20 09:30:44 +0200

    committing GPU02 remote only changes

--- baselines/her/command_lines/HandReach.sh
+++ baselines/her/command_lines/HandReach.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TRAIN="False"
-ENV="HandReach-v0"
+ENV="HandReach20-v0"
 #NUM_TIMESTEPS="10000"
 #N_EPOCHS="301"
 SAVE_VIDEO_INTERVAL="100000000"
@@ -10,10 +10,10 @@ MODE="maximum_span"
 MCA_EXPXLORATION="eps_greedy"
 MCA_ACTION_L2="0"
 SHARING="False"
-NUM_ENV="16"
+NUM_ENV="2"
 MAX_U="1"
 SS="False"
-DILUTE_AT_GOAL="False"
+DILUTE_AT_GOAL="True"
 
 k=0
 kmax=100
@@ -28,9 +28,18 @@ do
     #######################
     #### Learn k-Random cover
     RANDOM_COVER="True"
-    N_EPOCHS="11"
+    N_EPOCHS="1"
     LOG_PATH="${BASE_LOG_PATH}/${DATE}-${ENV}/K${k}/random"
-     xvfb-run -s "-screen 0 1400x900x24" /home/ron/nir/work/git/venvs/gym_venv_mj150/bin/python3.6 -m baselines.run --alg=her --env=$ENV\
+
+    echo "          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------
+          Starting new experiment. Logging directory ${LOG_PATH}
+          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------"
+
+     xvfb-run -a -s "-screen 0 1400x900x24" /home/ron/nir/work/git/venvs/gym_venv_mj150/bin/python3.6 -m baselines.run --alg=her --env=$ENV\
      --n_epochs=$N_EPOCHS --save_video_interval=$SAVE_VIDEO_INTERVAL --log_path=$LOG_PATH\
       --mode=$MODE --mca_exploration=$MCA_EXPXLORATION --mca_action_l2=$MCA_ACTION_L2 --sharing=$SHARING\
        --num_env=$NUM_ENV --max_u=$MAX_U --ss=$SS --k=$k --random_cover=$RANDOM_COVER --dilute_at_goal=$DILUTE_AT_GOAL
@@ -38,9 +47,18 @@ do
     #######################
     #### Learn k-MC cover
     RANDOM_COVER="False"
-    N_EPOCHS="221"
+    N_EPOCHS="1"
     LOG_PATH="${BASE_LOG_PATH}/${DATE}-${ENV}/K${k}/learned"
-    xvfb-run -s "-screen 0 1400x900x24" /home/ron/nir/work/git/venvs/gym_venv_mj150/bin/python3.6 -m baselines.run --alg=her --env=$ENV\
+
+    echo "          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------
+          Starting new experiment. Logging directory ${LOG_PATH}
+          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------
+          -------------------------------------------------------------------------------------"
+
+    xvfb-run -a -s "-screen 0 1400x900x24" /home/ron/nir/work/git/venvs/gym_venv_mj150/bin/python3.6 -m baselines.run --alg=her --env=$ENV\
      --n_epochs=$N_EPOCHS --save_video_interval=$SAVE_VIDEO_INTERVAL --log_path=$LOG_PATH\
       --mode=$MODE --mca_exploration=$MCA_EXPXLORATION --mca_action_l2=$MCA_ACTION_L2 --sharing=$SHARING\
        --num_env=$NUM_ENV --max_u=$MAX_U --ss=$SS --k=$k --random_cover=$RANDOM_COVER --dilute_at_goal=$DILUTE_AT_GOAL
